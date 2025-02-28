4e73c59e83a3bd0bbcfa1ba89cffb95c3e4cf941
Joshtri <stuffofyos1516@gmail.com>
2025-03-01 00:50:25 +0700

    Add AdminLayout component with sidebar and header for admin pages

---
+++ app/(admin)/layout.js
@@ -0,0 +1,26 @@
+"use client";
+
+import React, { useState } from "react";
+import AdminHeader from "@/components/admin/partials/header";
+import AdminSidebar from "@/components/admin/partials/sidebar";
+import AdminFooter from "@/components/admin/partials/footer";
+import ClientThemeProvider from "@/providers/client-theme-provider";
+
+export default function AdminLayout({ children }) {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  const toggleSidebar = () => setIsSidebarOpen((prev) => !prev);
+
+  return (
+    <ClientThemeProvider>
+      <div className="flex">
+        <AdminSidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+        <div className={`flex-1 flex flex-col min-h-screen transition-all duration-300 ${isSidebarOpen ? "ml-64" : "ml-20"}`}>
+          <AdminHeader toggleSidebar={toggleSidebar} />
+          <main className="mt-20 p-6">{children}</main>
+          <AdminFooter />
+        </div>
+      </div>
+    </ClientThemeProvider>
+  );
+}
