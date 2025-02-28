77e37641a63c724075959b3100e45d58479c4aff
Joshtri <stuffofyos1516@gmail.com>
2025-02-28 09:22:46 +0700

    Add Dashboard Pelapor page with metadata and component integration

---
+++ app/(masyarakat)/pelapor/dashboard/page.js
@@ -0,0 +1,16 @@
+import DashboardPelapor from '@/components/pelapor/pelapor-dashboard'
+import React from 'react'
+
+
+export const metadata = {
+  title: 'Dashboard Pelapor',
+  description: 'Dashboard Pelapor',
+}
+
+
+export default function DashboardPelaporPage() {
+  return (
+    <DashboardPelapor/>
+     
+  )
+}
