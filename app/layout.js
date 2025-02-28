f2e890d3d9cd78fe14c76aa5ac9468e40e9973a2
Joshtri <stuffofyos1516@gmail.com>
2025-02-28 09:22:34 +0700

    Comment out Header component in RootLayout

--- app/layout.js
+++ app/layout.js
@@ -28,7 +28,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ClientThemeProvider>
-          <Header/>
+          {/* <Header/> */}
           <FloatingHelper/>
           {children}
         </ClientThemeProvider>
