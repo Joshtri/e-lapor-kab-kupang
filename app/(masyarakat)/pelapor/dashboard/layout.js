7da99b50db9579c7bd8b64f10b91b0ee1eb03bc2
Joshtri <stuffofyos1516@gmail.com>
2025-02-28 23:50:22 +0700

    Add dashboard layout component with metadata for Pelapor

---
+++ app/(masyarakat)/pelapor/dashboard/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "Dashboard Pelapor",
+  description: "Dashboard Pelapor",
+};
+
+export default function DashboardLayout({ children }) {
+  return <>{children}</>;
+}
