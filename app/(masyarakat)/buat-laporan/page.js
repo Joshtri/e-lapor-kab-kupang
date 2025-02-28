28ef8c579ae767f64db1a739cd5bc1710ad550af
Joshtri <stuffofyos1516@gmail.com>
2025-02-28 12:46:19 +0700

    Add BuatLaporanPage component for creating reports

---
+++ app/(masyarakat)/buat-laporan/page.js
@@ -0,0 +1,8 @@
+import BuatLaporan from '@/components/pelapor/create-report-pelapor'
+import React from 'react'
+
+export default function BuatLaporanPage() {
+  return (
+    <BuatLaporan/>  
+  )
+}
