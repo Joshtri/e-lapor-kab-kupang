7c22c70f5a8e5905b622ac3b5b7aace1c98025eb
Joshtri <stuffofyos1516@gmail.com>
2025-02-28 23:16:46 +0700

    Remove min-height from ClientThemeProvider layout for improved flexibility

--- providers/client-theme-provider.jsx
+++ providers/client-theme-provider.jsx
@@ -7,7 +7,7 @@ export default function ClientThemeProvider({ children }) {
   return (
     <ThemeProvider attribute="class" enableSystem>
       <Flowbite>
-        <div className="min-h-screen bg-white dark:bg-gray-900 transition-colors">
+        <div className="bg-white dark:bg-gray-900 transition-colors">
           {children}
         </div>
       </Flowbite>
