e37129d200d96f4efec74e494bb5bc72b229de2d
Joshtri <stuffofyos1516@gmail.com>
2025-03-01 08:10:43 +0700

    Update ESLint configuration, enhance Next.js settings, and add Prisma generate scripts

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    // Enable middleware
+    experimental: {
+      middleware: true
+    },
+    images: {
+      domains: ['firebasestorage.googleapis.com'],
+    },
+  }
 
 export default nextConfig;
