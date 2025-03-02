68e7adff67fe512737edce776e3f3a650957ab2b
Joshtri <stuffofyos1516@gmail.com>
2025-03-02 16:44:56 +0700

    Add Toaster component to AdminLayout for improved user notifications

--- app/(admin)/layout.js
+++ app/(admin)/layout.js
@@ -5,6 +5,7 @@ import AdminHeader from "@/components/admin/partials/header";
 import AdminSidebar from "@/components/admin/partials/sidebar";
 import AdminFooter from "@/components/admin/partials/footer";
 import ClientThemeProvider from "@/providers/client-theme-provider";
+import { Toaster } from "sonner";
 
 export default function AdminLayout({ children }) {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
@@ -13,9 +14,14 @@ export default function AdminLayout({ children }) {
 
   return (
     <ClientThemeProvider>
+      <Toaster richColors position="top-right" />
       <div className="flex">
         <AdminSidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-        <div className={`flex-1 flex flex-col min-h-screen transition-all duration-300 ${isSidebarOpen ? "ml-64" : "ml-20"}`}>
+        <div
+          className={`flex-1 flex flex-col min-h-screen transition-all duration-300 ${
+            isSidebarOpen ? "ml-64" : "ml-20"
+          }`}
+        >
           <AdminHeader toggleSidebar={toggleSidebar} />
           <main className="mt-20 p-6">{children}</main>
           <AdminFooter />
