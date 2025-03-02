bca6e6fafe1059dc7f120b08dc8347b4961751ec
Joshtri <stuffofyos1516@gmail.com>
2025-03-02 14:28:36 +0700

    Update header component to change dashboard link and add navigation menu

--- components/bupati/partials/header.jsx
+++ components/bupati/partials/header.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import { Navbar, Button, Dropdown, Avatar, Modal } from "flowbite-react";
+import { Navbar, Button, Dropdown, Avatar, Modal, NavbarLink } from "flowbite-react";
 import { BsMoonStarsFill, BsSunFill } from "react-icons/bs";
 import { HiOutlineUserCircle, HiOutlineLogout, HiOutlineHome, HiOutlineClipboardCheck } from "react-icons/hi";
 import { useTheme } from "next-themes";
@@ -30,12 +30,22 @@ const HeaderBupati = ({ user }) => {
   return (
     <Navbar fluid rounded className="py-4 px-6 bg-white dark:bg-gray-800 shadow-lg fixed w-full z-40 top-0">
       {/* Brand Logo */}
-      <Navbar.Brand href="/bupati/dashboard">
+      <Navbar.Brand href="/bupati-portal/dashboard">
         <HiOutlineHome className="h-8 w-8 text-blue-600 dark:text-blue-400" />
         <span className="text-2xl font-bold text-gray-800 dark:text-gray-200 ml-2">Dashboard Bupati</span>
       </Navbar.Brand>
 
-      {/* Navigasi */}
+      {/* Menu Navigasi */}
+      <Navbar.Collapse>
+        <NavbarLink as={Link} href="/bupati-portal/laporan" className="text-gray-800 dark:text-gray-200 hover:text-blue-500">
+          Daftar Laporan
+        </NavbarLink>
+        <NavbarLink as={Link} href="/bupati-portal/statistik" className="text-gray-800 dark:text-gray-200 hover:text-blue-500">
+          Statistik
+        </NavbarLink>
+      </Navbar.Collapse>
+
+      {/* Aksi Kanan */}
       <div className="flex md:order-2 items-center space-x-3">
         {/* Tombol Dark Mode */}
         <button
@@ -51,7 +61,7 @@ const HeaderBupati = ({ user }) => {
           arrowIcon={false}
           inline
           label={
-            <Avatar alt="User" img="/images/avatar-bupati.png" rounded size="sm" />
+            <Avatar alt="User" img="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiPz4KPHN2ZyB2aWV3Qm94PSIwIDAgMTI4IDEyOCIgdmVyc2lvbj0iMS4xIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHJvbGU9ImltZyIgYXJpYS1sYWJlbD0ieHhsYXJnZSI+CiAgICA8Zz4KICAgICAgICA8Y2lyY2xlIGN4PSI2NCIgY3k9IjY0IiByPSI2NCIgZmlsbD0iIzg5OTNhNCIgLz4KICAgICAgICA8Zz4KICAgICAgICAgICAgPHBhdGggZmlsbD0iI2ZmZiIKICAgICAgICAgICAgICAgIGQ9Ik0xMDMsMTAyLjEzODggQzkzLjA5NCwxMTEuOTIgNzkuMzUwNCwxMTggNjQuMTYzOCwxMTggQzQ4LjgwNTYsMTE4IDM0LjkyOTQsMTExLjc2OCAyNSwxMDEuNzg5MiBMMjUsOTUuMiBDMjUsODYuODA5NiAzMS45ODEsODAgNDAuNiw4MCBMODcuNCw4MCBDOTYuMDE5LDgwIDEwMyw4Ni44MDk2IDEwMyw5NS4yIEwxMDMsMTAyLjEzODggWiIgLz4KICAgICAgICAgICAgPHBhdGggZmlsbD0iI2ZmZiIKICAgICAgICAgICAgICAgIGQ9Ik02My45OTYxNjQ3LDI0IEM1MS4yOTM4MTM2LDI0IDQxLDM0LjI5MzgxMzYgNDEsNDYuOTk2MTY0NyBDNDEsNTkuNzA2MTg2NCA1MS4yOTM4MTM2LDcwIDYzLjk5NjE2NDcsNzAgQzc2LjY5ODUxNTksNzAgODcsNTkuNzA2MTg2NCA4Nyw0Ni45OTYxNjQ3IEM4NywzNC4yOTM4MTM2IDc2LjY5ODUxNTksMjQgNjMuOTk2MTY0NywyNCIgLz4KICAgICAgICA8L2c+CiAgICA8L2c+Cjwvc3ZnPgo=" rounded size="sm" />
           }
         >
           <Dropdown.Header>
