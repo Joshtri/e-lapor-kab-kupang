8347b9c777f62e94f964a9bdadf27f2c9d143de6
Joshtri <stuffofyos1516@gmail.com>
2025-03-25 21:46:37 +0700

    Enhance RiwayatTable component to display reporter name and OPD status with updated badge logic

--- components/admin/riwayat-report/riwayat-report-table-view.jsx
+++ components/admin/riwayat-report/riwayat-report-table-view.jsx
@@ -6,30 +6,51 @@ export default function RiwayatTable({ riwayat }) {
   return (
     <Table striped>
       <Table.Head>
+        <Table.HeadCell>Nama Pelapor</Table.HeadCell>
         <Table.HeadCell>Subjek Laporan</Table.HeadCell>
+        <Table.HeadCell>OPD Dituju</Table.HeadCell>
         <Table.HeadCell>Kategori</Table.HeadCell>
-        <Table.HeadCell>Status</Table.HeadCell>
+        <Table.HeadCell>Bupati Status</Table.HeadCell>
+        <Table.HeadCell>OPD Status</Table.HeadCell>
         <Table.HeadCell>Prioritas</Table.HeadCell>
         <Table.HeadCell>Tanggal</Table.HeadCell>
       </Table.Head>
       <Table.Body>
         {riwayat.map((item) => (
           <Table.Row key={item.id}>
+            <Table.Cell>{item.user.name}</Table.Cell>
             <Table.Cell>{item.title}</Table.Cell>
+            <Table.Cell>{item.opd.name}</Table.Cell>
             <Table.Cell>{item.category}</Table.Cell>
             <Table.Cell>
               <Badge
                 color={
-                  item.status === "SELESAI"
+                  item.bupatiStatus === "SELESAI"
                     ? "green"
-                    : item.status === "PROSES"
+                    : item.bupatiStatus === "PROSES"
                       ? "yellow"
-                      : item.status === "DITOLAK"
+                      : item.bupatiStatus === "DITOLAK"
                         ? "red"
                         : "gray"
                 }
               >
-                {item.status}
+                {item.bupatiStatus}
+              </Badge>
+            </Table.Cell>
+
+            <Table.Cell>
+              <Badge
+                color={
+                  item.opdStatus === "SELESAI"
+                    ? "green"
+                    : item.opdStatus === "PROSES"
+                      ? "yellow"
+                      : item.opdStatus === "DITOLAK"
+                        ? "red"
+                        : "gray"
+                }
+              >
+                {item.opdStatus}
               </Badge>
             </Table.Cell>
             <Table.Cell>{item.priority}</Table.Cell>
