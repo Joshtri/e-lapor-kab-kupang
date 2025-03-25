66ca17decaa3e2127f9823d0947a94f29f60533c
Joshtri <stuffofyos1516@gmail.com>
2025-03-25 23:39:52 +0700

    Refactor layout.js to improve code consistency and formatting

--- app/(public)/layout.js
+++ app/(public)/layout.js
@@ -1,12 +1,11 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "../globals.css";
-import Header from "@/components/partials/header";
-import ScrollToTopButton from "@/components/ui/scroll-to-top-button";
-
+import { Geist, Geist_Mono } from 'next/font/google';
+import '../globals.css';
+import Header from '@/components/partials/header';
+import ScrollToTopButton from '@/components/ui/scroll-to-top-button';
 
 export const metadata = {
-  title: "LAPOR KK BUPATI",
-  description: "Sampaikan laporan Anda melalui LAPOR KK BUPATI",
+  title: 'LAPOR KK BUPATI',
+  description: 'Sampaikan laporan Anda melalui LAPOR KK BUPATI',
 };
 
 export default function PublicLayout({ children }) {
@@ -14,8 +13,6 @@ export default function PublicLayout({ children }) {
     <div className="min-h-screen flex flex-col">
       <Header />
       <main className="flex-grow">{children}</main>
-
-
     </div>
   );
 }
