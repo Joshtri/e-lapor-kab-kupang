5507f1fc4d12219bc333d704e46f03fa995d825b
Joshtri <stuffofyos1516@gmail.com>
2025-03-25 23:39:59 +0700

    Refactor PelaporLayout component to improve code consistency, update styling, and enhance layout

--- app/(masyarakat)/layout.js
+++ app/(masyarakat)/layout.js
@@ -1,30 +1,30 @@
-import { cookies } from "next/headers";
-import { verify } from "jsonwebtoken";
-import { redirect } from "next/navigation";
-import { ThemeProvider } from "next-themes";
-import HeaderPelapor from "@/components/pelapor/partials/header";
-import FooterPelapor from "@/components/pelapor/partials/footer";
-import { Toaster } from "sonner";
+import { cookies } from 'next/headers';
+import { verify } from 'jsonwebtoken';
+import { redirect } from 'next/navigation';
+import { ThemeProvider } from 'next-themes';
+import HeaderPelapor from '@/components/pelapor/partials/header';
+import FooterPelapor from '@/components/pelapor/partials/footer';
+import { Toaster } from 'sonner';
 
 export default function PelaporLayout({ children }) {
-  const token = cookies().get("auth_token")?.value;
+  const token = cookies().get('auth_token')?.value;
 
   if (!token) {
-    redirect("/auth/login");
+    redirect('/auth/login');
   }
 
   try {
     verify(token, process.env.JWT_SECRET);
   } catch (error) {
-    redirect("/auth/login");
+    redirect('/auth/login');
   }
 
   return (
     <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
-      <div className="flex flex-col min-h-screen bg-gray-100 dark:bg-gray-900">
+      <div className="flex flex-col min-h-screen bg-blue-50  dark:bg-gray-900">
         <Toaster richColors position="top-right" />
         <HeaderPelapor />
-        <main className="flex-grow p-4 pt-16">{children}</main>
+        <main className="flex-grow pt-14">{children}</main>
         <FooterPelapor />
       </div>
     </ThemeProvider>
