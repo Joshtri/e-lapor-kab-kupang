32b216b5737276605d73efb40cf7b646f7d299fa
Joshtri <stuffofyos1516@gmail.com>
2025-03-25 12:20:40 +0700

    Refactor import statements in DashboardAdminPage for consistency

--- app/(admin)/adm/dashboard/page.js
+++ app/(admin)/adm/dashboard/page.js
@@ -1,5 +1,5 @@
-import AdminDashboard from "@/components/admin/dashboard-admin";
-import React from "react";
+import AdminDashboard from '@/components/admin/dashboard-admin';
+import React from 'react';
 
 export default function DashboardAdminPage() {
   return <AdminDashboard />;
