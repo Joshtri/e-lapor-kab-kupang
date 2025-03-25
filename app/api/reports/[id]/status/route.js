baf15c27b2c08c56c96a67e2725c85039879753d
Joshtri <stuffofyos1516@gmail.com>
2025-03-25 12:22:05 +0700

    Refactor PATCH route to update report status: rename status variable to bupatiStatus and standardize string quotes

--- app/api/reports/[id]/status/route.js
+++ app/api/reports/[id]/status/route.js
@@ -1,24 +1,24 @@
-import prisma from "@/lib/prisma";
+import prisma from '@/lib/prisma';
 
 export async function PATCH(req, { params }) {
   const { id } = params; // reportId
-  const { status } = await req.json();
+  const { bupatiStatus } = await req.json();
 
-  if (!status) {
-    return Response.json({ error: "Status wajib diisi." }, { status: 400 });
+  if (!bupatiStatus) {
+    return Response.json({ error: 'Status wajib diisi.' }, { status: 400 });
   }
 
   try {
     const updatedReport = await prisma.report.update({
       where: { id: Number(id) },
-      data: { status },
+      data: { bupatiStatus },
     });
 
     return Response.json(updatedReport);
   } catch (error) {
     console.error(error);
     return Response.json(
-      { error: "Gagal mengupdate status laporan." },
+      { error: 'Gagal mengupdate status laporan.' },
       { status: 500 },
     );
   }
