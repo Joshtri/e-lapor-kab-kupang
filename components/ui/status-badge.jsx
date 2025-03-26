b6743012dcd7b1c26c4934aa3013ddc2b79ca8e1
Joshtri <stuffofyos1516@gmail.com>
2025-03-26 13:19:00 +0700

    Refactor StatusBadge component to enhance visual representation and add icons for different statuses

--- components/ui/status-badge.jsx
+++ components/ui/status-badge.jsx
@@ -1,20 +1,49 @@
-"use client";
-
 import React from "react";
 
-const statusClasses = {
-  SELESAI: "bg-green-500",
-  PROSES: "bg-yellow-500",
-  PENDING: "bg-gray-500",
-  DITOLAK: "bg-red-500",
-  DEFAULT: "bg-gray-400",
-};
+
 
 const StatusBadge = ({ status }) => {
-  const badgeClass = statusClasses[status] || statusClasses.DEFAULT;
-  
+  let color = "bg-gray-100 text-gray-800";
+  let icon = null;
+
+  switch (status?.toLowerCase()) {
+    case "SELESAI":
+      color = "bg-green-100 text-green-800 border-green-200";
+      icon = (
+        <svg className="w-3 h-3 mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+          <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path>
+        </svg>
+      );
+      break;
+    case "PROSES":
+      color = "bg-yellow-100 text-yellow-800 border-yellow-200";
+      icon = (
+        <svg className="w-3 h-3 mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+          <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clipRule="evenodd"></path>
+        </svg>
+      );
+      break;
+    case "DITOLAK":
+      color = "bg-red-100 text-red-800 border-red-200";
+      icon = (
+        <svg className="w-3 h-3 mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+          <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path>
+        </svg>
+      );
+      break;
+    case "PENDING":
+      color = "bg-blue-100 text-blue-800 border-blue-200";
+      icon = (
+        <svg className="w-3 h-3 mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+          <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd"></path>
+        </svg>
+      );
+      break;
+  }
+
   return (
-    <span className={`px-3 py-1 rounded-full text-white text-sm ${badgeClass}`}>
+    <span className={`inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium border ${color}`}>
+      {icon}
       {status}
     </span>
   );
