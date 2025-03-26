77569e4a3b8c942e028f2a55934b8de6b2bc4c09
Joshtri <stuffofyos1516@gmail.com>
2025-03-26 13:20:11 +0700

    Refactor DashboardBupatiPage to standardize string quotes and update component imports

--- app/(bupati)/bupati-portal/dashboard/page.js
+++ app/(bupati)/bupati-portal/dashboard/page.js
@@ -1,12 +1,11 @@
-"use client";
+'use client';
 
-import React from "react";
+import React from 'react';
 // import StatistikOverview from "@/components/bupati/statistic-overview";
-import BupatiDashboard from "@/components/bupati/dashboard-bupati";
+import BupatiDashboard from '@/components/bupati/DashboardBupati';
+import AdminDashboard from '@/components/admin/dashboard-admin';
 
 export default function DashboardBupatiPage() {
-  return (
- 
-    <BupatiDashboard/>
-  );
+  return <AdminDashboard  titleHeader={'Dashboard Bupati'}/>;
+  // return <BupatiDashboard />;
 }
