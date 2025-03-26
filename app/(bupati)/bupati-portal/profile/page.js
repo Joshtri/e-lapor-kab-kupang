d9e239261eddc5c92fe284e763f1ca2e142ee1cb
Joshtri <stuffofyos1516@gmail.com>
2025-03-26 13:20:23 +0700

    Enhance ProfileManagementPage by adding PageHeader for improved navigation and layout

--- app/(bupati)/bupati-portal/profile/page.js
+++ app/(bupati)/bupati-portal/profile/page.js
@@ -1,8 +1,26 @@
-import ProfileManagement from '@/components/profile'
-import React from 'react'
+import ProfileManagement from '@/components/profile';
+import PageHeader from '@/components/ui/page-header';
+import React from 'react';
 
 export default function ProfileManagementPage() {
   return (
-    <ProfileManagement/>
-  )
+    <>
+      <div className="max-w-7xl mx-auto p-6  mt-10">
+        <PageHeader
+          title={'Profil Saya'}
+          backHref="/bupati-portal/dashboard"
+          breadcrumbsProps={{
+            home: { label: 'Beranda', href: '/bupati-portal/dashboard' },
+            customRoutes: {
+              'bupati-portal': {
+                label: 'Dashboard bupati-portal',
+                href: '/bupati-portal/dashboard',
+              },
+            },
+          }}
+        />
+      </div>
+      <ProfileManagement />
+    </>
+  );
 }
