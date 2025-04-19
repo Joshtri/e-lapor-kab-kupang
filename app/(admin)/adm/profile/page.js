12ddb64ffc09d245263cfd4955b65ea04641350d
Joshtri <stuffofyos1516@gmail.com>
2025-04-19 12:48:36 +0700

    refactor: Correct import path for PageHeader component to match naming convention

--- app/(admin)/adm/profile/page.js
+++ app/(admin)/adm/profile/page.js
@@ -1,12 +1,12 @@
 import ProfileManagement from '@/components/profile';
-import PageHeader from '@/components/ui/page-header';
+import PageHeader from '@/components/ui/PageHeader';
 
 export default function ProfileManagementPage() {
   return (
     <>
       <div className="max-w-7xl mx-auto p-6  mt-10">
         <PageHeader
-           backHref="/adm/dashboard"
+          backHref="/adm/dashboard"
           breadcrumbsProps={{
             home: { label: 'Beranda', href: '/adm/dashboard' },
             customRoutes: {
