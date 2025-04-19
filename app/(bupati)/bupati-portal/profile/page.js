291901d449dd50f1379eaff1725fc8e0df69a37b
Joshtri <stuffofyos1516@gmail.com>
2025-04-19 12:48:44 +0700

    refactor: Correct import path for PageHeader component to match naming convention

--- app/(bupati)/bupati-portal/profile/page.js
+++ app/(bupati)/bupati-portal/profile/page.js
@@ -1,5 +1,5 @@
 import ProfileManagement from '@/components/profile';
-import PageHeader from '@/components/ui/page-header';
+import PageHeader from '@/components/ui/PageHeader';
 import React from 'react';
 
 export default function ProfileManagementPage() {
