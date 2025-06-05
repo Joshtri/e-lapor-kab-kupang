4d66029e2bf188d00df858c7aa2add6f83272fd0
Joshtri <stuffofyos1516@gmail.com>
2025-06-06 01:36:57 +0700

    feat: Add metadata for Forgot Password page

--- app/(public)/auth/forgot-password/page.js
+++ app/(public)/auth/forgot-password/page.js
@@ -1,5 +1,10 @@
 import ForgetPassword from '@/components/forget-password';
 
+export const metadata = {
+  title: 'Lupa Password | Lapor KK Bupati',
+  description: 'Atur ulang password Anda untuk kembali mengakses akun.',
+};
+
 export default function ForgotPasswordPage() {
   return <ForgetPassword />;
 }
