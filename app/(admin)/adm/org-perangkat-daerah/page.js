f63f5f516deaa3ded893b4546292a2e428bde8e9
Joshtri <stuffofyos1516@gmail.com>
2025-06-06 01:37:46 +0700

    feat: Add metadata for OPD page with title and description

--- app/(admin)/adm/org-perangkat-daerah/page.js
+++ app/(admin)/adm/org-perangkat-daerah/page.js
@@ -1,6 +1,9 @@
 import OPDList from '@/components/admin/opd/OpdList'
-import React from 'react'
 
+export const metadata = {
+  title: 'Data OPD | Lapor KK Bupati',
+  description: 'Daftar Organisasi Perangkat Daerah pada Lapor KK Bupati.',
+};
 export default function OpdPage() {
   return (
     <OPDList/>
