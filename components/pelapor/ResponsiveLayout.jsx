32da33c71647ef6d5d50e9adae90ee447fc53b1a
Joshtri <stuffofyos1516@gmail.com>
2025-06-07 04:03:22 +0700

    refactor: Standardize import formatting and clean up unused session handling in ResponsiveLayout component

--- components/pelapor/ResponsiveLayout.jsx
+++ components/pelapor/ResponsiveLayout.jsx
@@ -1,22 +1,26 @@
-"use client"
+'use client';
 
-import { useIsMobile } from "@/hooks/use-media-query"
-import HeaderPelapor from "@/components/pelapor/partials/header"
-import FooterPelapor from "@/components/pelapor/partials/footer"
-import HeaderMobile from "@/components/pelapor/partials/HeaderPelaporMobile"
-import FooterMobile from "@/components/pelapor/partials/FooterPelaporMobile"
-import { useSession } from "next-auth/react"
+import { useIsMobile } from '@/hooks/use-media-query';
+import HeaderPelapor from '@/components/pelapor/partials/header';
+import FooterPelapor from '@/components/pelapor/partials/footer';
+import HeaderMobile from '@/components/pelapor/partials/HeaderPelaporMobile';
+import FooterMobile from '@/components/pelapor/partials/FooterPelaporMobile';
+import PropTypes from 'prop-types';
 
 export default function ResponsiveLayout({ children }) {
-  const isMobile = useIsMobile()
-  const { data: session } = useSession()
-  const user = session?.user
+  const isMobile = useIsMobile();
 
   return (
     <div className="flex flex-col min-h-screen bg-blue-50 dark:bg-gray-900">
-      {isMobile ? <HeaderMobile user={user} /> : <HeaderPelapor />}
-      <main className={`flex-grow ${isMobile ? "pt-14 pb-16" : "pt-16"}`}>{children}</main>
+      {isMobile ? <HeaderMobile /> : <HeaderPelapor />}
+      <main className={`flex-grow ${isMobile ? 'pt-14 pb-16' : 'pt-16'}`}>
+        {children}
+      </main>
       {isMobile ? <FooterMobile /> : <FooterPelapor />}
     </div>
-  )
+  );
 }
+
+ResponsiveLayout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
