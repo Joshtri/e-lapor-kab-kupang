dc358ee4f97cbc3f8513bf0fa4ed0bcf25e4e73c
Joshtri <stuffofyos1516@gmail.com>
2025-06-07 04:03:11 +0700

    fix: Adjust position of floating button in FloatingHelper component

--- components/floating-helpers.jsx
+++ components/floating-helpers.jsx
@@ -19,7 +19,7 @@ export default function FloatingHelper() {
     <>
       {/* Floating Button */}
       <motion.div
-        className="fixed bottom-20 right-6 z-50"
+        className="fixed bottom-6 right-6 z-50"
         initial={{ opacity: 0, scale: 0 }}
         animate={{ opacity: 1, scale: 1 }}
         transition={{ duration: 0.3 }}
