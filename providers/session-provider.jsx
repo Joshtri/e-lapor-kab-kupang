9cedf8f7f2967d454b531a8d61947e8d111ca994
Joshtri <stuffofyos1516@gmail.com>
2025-06-07 04:02:51 +0700

    feat: Add PropTypes validation for children in NextAuthSessionProvider component

--- providers/session-provider.jsx
+++ providers/session-provider.jsx
@@ -1,7 +1,12 @@
 "use client"
 
 import { SessionProvider } from "next-auth/react"
+import PropTypes from 'prop-types'
 
 export default function NextAuthSessionProvider({ children }) {
   return <SessionProvider>{children}</SessionProvider>
+}
+
+NextAuthSessionProvider.propTypes = {
+  children: PropTypes.node.isRequired
 }
