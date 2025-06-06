32d66d804bf47a4c758498544da8be4ff90e9059
Joshtri <stuffofyos1516@gmail.com>
2025-06-07 01:14:53 +0700

    feat: Add StatistikPage component with metadata for Statistik Pelapor

---
+++ app/(masyarakat)/pelapor/statistik/page.js
@@ -0,0 +1,11 @@
+// app/pelapor/statistik/page.jsx
+import StatistikPelaporPage from '@/views/pelapor/StatistikPelaporPage';
+
+export const metadata = {
+  title: 'Statistik Pelapor | Lapor KK Bupati',
+  description: 'Halaman statistik untuk pelapor',
+};
+
+export default function StatistikPage() {
+  return <StatistikPelaporPage />; // ✅ tanpa prop user
+}
