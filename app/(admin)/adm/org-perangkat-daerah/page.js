5d2cd1ba51626d5d716263409d57107a98d80d70
Joshtri <stuffofyos1516@gmail.com>
2025-11-21 12:00:52 +0700

    Commit: page.js

--- app/(admin)/adm/org-perangkat-daerah/page.js
+++ app/(admin)/adm/org-perangkat-daerah/page.js
@@ -1,9 +1,9 @@
-import OPDList from '@/components/admin/opd/OpdList';
+import KelolaOrganisasiList from '@/features/kelola-organisasi-perangkat-daerah/list';
 
 export const metadata = {
   title: 'Data OPD | Lapor Kaka Bupati',
   description: 'Daftar Organisasi Perangkat Daerah pada Lapor Kaka Bupati.',
 };
-export default function OpdPage() {
-  return <OPDList />;
+export default function OpdListPage() {
+  return <KelolaOrganisasiList />;
 }
