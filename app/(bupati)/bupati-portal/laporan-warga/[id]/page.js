4c068afd73dd91057b6cc2e0a1dd55cb54ea5b5f
Joshtri <stuffofyos1516@gmail.com>
2025-11-21 12:00:53 +0700

    Commit: page.js

--- app/(bupati)/bupati-portal/laporan-warga/[id]/page.js
+++ app/(bupati)/bupati-portal/laporan-warga/[id]/page.js
@@ -1,6 +1,6 @@
-import ReportView from '@/components/bupati/report/ReportDetail';
+import PengaduanDetailBupati from '@/features/pengaduan/bupati/detail';
 import React from 'react';
 
 export default function LaporanWargaView() {
-  return <ReportView />;
+  return <PengaduanDetailBupati />;
 }
