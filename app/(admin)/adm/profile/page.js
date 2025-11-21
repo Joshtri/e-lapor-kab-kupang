44be10891a14e4ac0a58a3eb609019eb252d1887
Joshtri <stuffofyos1516@gmail.com>
2025-11-21 12:00:52 +0700

    Commit: page.js

--- app/(admin)/adm/profile/page.js
+++ app/(admin)/adm/profile/page.js
@@ -11,17 +11,7 @@ export default function ProfileManagementPage() {
   return (
     <>
       <div className="max-w-7xl mx-auto p-6  mt-10">
-        <PageHeader
-          backHref="/adm/dashboard"
-          breadcrumbsProps={{
-            customRoutes: {
-              adm: {
-                label: 'Dashboard Admin',
-                href: '/adm/dashboard',
-              },
-            },
-          }}
-        />
+        <PageHeader backHref="/adm/dashboard" title="Profile Management" />
       </div>
       <ProfileManagement />
     </>
