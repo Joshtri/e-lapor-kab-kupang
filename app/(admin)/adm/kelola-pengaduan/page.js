bc458159acff920fad6d6c9c5d7e52d53858e6cc
Joshtri <stuffofyos1516@gmail.com>
2025-11-21 12:00:52 +0700

    Commit: page.js

--- app/(admin)/adm/kelola-pengaduan/page.js
+++ app/(admin)/adm/kelola-pengaduan/page.js
@@ -1,5 +1,7 @@
-import ReportList from '@/components/admin/report/ReportList';
+// import ReportList from '@/components/admin/report/ReportList';
+
+import PengaduanList from '@/features/pengaduan/admin/list';
 
 export default function ReportsPage() {
-  return <ReportList />;
+  return <PengaduanList />;
 }
