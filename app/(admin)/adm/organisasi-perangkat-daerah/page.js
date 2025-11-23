71455c399fcd015441aacfc30d2636ee889808eb
Joshtri <stuffofyos1516@gmail.com>
2025-11-23 23:59:05 +0700

    Commit: page.js

---
+++ app/(admin)/adm/organisasi-perangkat-daerah/page.js
@@ -0,0 +1,6 @@
+import KelolaOrganisasiListV2 from '@/features/kelola-organisasi-perangkat-daerah-v2/list';
+import React from 'react';
+
+export default function OrganisasiPerangkatDaerahPage() {
+  return <KelolaOrganisasiListV2 />;
+}
