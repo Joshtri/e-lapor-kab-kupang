925956e4af857c03c311667cf739dfcb3018d18b
Joshtri <stuffofyos1516@gmail.com>
2025-11-23 23:59:08 +0700

    Commit: page.js

---
+++ app/(admin)/adm/users/staff-opd/page.js
@@ -0,0 +1,10 @@
+import StaffOpdList from '@/features/kelola-pengguna/staff-opd/list';
+
+export const metadata = {
+  title: 'Kelola Akun Staff OPD',
+  description: 'Manajemen akun Staff Organisasi Perangkat Daerah',
+};
+
+export default function StaffOpdPage() {
+  return <StaffOpdList />;
+}
