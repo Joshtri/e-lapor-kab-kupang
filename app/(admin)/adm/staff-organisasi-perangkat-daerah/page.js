c120c5e83e6f0c1674279ed94f66b58b354050ac
Joshtri <stuffofyos1516@gmail.com>
2025-11-23 23:59:05 +0700

    Commit: page.js

---
+++ app/(admin)/adm/staff-organisasi-perangkat-daerah/page.js
@@ -0,0 +1,10 @@
+import StaffOpdList from '@/features/kelola-staff-organisasi-perangkat-daerah/list';
+
+export const metadata = {
+  title: 'Manajemen Staff OPD',
+  description: 'Kelola daftar staff Organisasi Perangkat Daerah',
+};
+
+export default function StaffOpdPage() {
+  return <StaffOpdList />;
+}
