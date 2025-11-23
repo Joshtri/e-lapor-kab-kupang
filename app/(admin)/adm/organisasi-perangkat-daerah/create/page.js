85345d67ef0f9ba516ac4ebd64f4916dd8089620
Joshtri <stuffofyos1516@gmail.com>
2025-11-23 23:59:05 +0700

    Commit: page.js

---
+++ app/(admin)/adm/organisasi-perangkat-daerah/create/page.js
@@ -0,0 +1,6 @@
+import OpdForm from '@/features/kelola-organisasi-perangkat-daerah-v2/form/OpdForm';
+import React from 'react';
+
+export default function OrganisasiPerangkatDaerahCreatePage() {
+  return <OpdForm />;
+}
