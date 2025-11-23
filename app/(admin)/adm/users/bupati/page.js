cb6244c5d133e5cee417c598b536acc240a9286e
Joshtri <stuffofyos1516@gmail.com>
2025-11-23 23:59:07 +0700

    Commit: page.js

---
+++ app/(admin)/adm/users/bupati/page.js
@@ -0,0 +1,10 @@
+import BupatiList from '@/features/kelola-pengguna/bupati/list';
+
+export const metadata = {
+  title: 'Kelola Akun Bupati',
+  description: 'Manajemen akun Bupati (Kepala Daerah) e-Lapor',
+};
+
+export default function BupatiPage() {
+  return <BupatiList />;
+}
