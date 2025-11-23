cfed379ca9f3efe6ae41aa2889cd83096fb0c5c1
Joshtri <stuffofyos1516@gmail.com>
2025-11-23 23:59:07 +0700

    Commit: page.js

---
+++ app/(admin)/adm/users/pelapor/page.js
@@ -0,0 +1,10 @@
+import PelaporList from '@/features/kelola-pengguna/pelapor/list';
+
+export const metadata = {
+  title: 'Kelola Akun Pelapor',
+  description: 'Manajemen akun masyarakat (pelapor) e-Lapor',
+};
+
+export default function PelaporPage() {
+  return <PelaporList />;
+}
